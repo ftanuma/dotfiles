d079d1aaa35c7b39b914b5fcf64a169e3b3b1b7b
Fumihiko Tanuma <ftanuma@gmail.com>
2013-05-11 22:14:09 +0900

    Modified theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -60,7 +60,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; P65 CUIとGUIによる分岐
 ;; ターミナル以外はツールバー、スクロールバーを非表示
-(when window-system
+;;(when window-system
   ;; tool-barを非表示
 ;;  (tool-bar-mode 0)
   ;; scroll-barを非表示
@@ -182,7 +182,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; P95-96 フェイス
 ;; リージョンの背景色を変更
-;; (set-face-background 'region "darkgreen")
+;;(set-face-background 'region "darkgreen")
 
 ;; ▼要拡張機能インストール▼
 ;;; P96-97 表示テーマの設定
@@ -191,45 +191,51 @@
   ;; テーマを読み込むための設定
   (color-theme-initialize)
   ;; テーマhoberに変更する
-  (color-theme-hober))
+  (color-theme-clarity))
+
 
 ;;; P97-99 フォントの設定
+;; for Mac
 (when (eq window-system 'ns)
   ;; asciiフォントをMenloに
   (set-face-attribute 'default nil
                       :family "Menlo"
-                      :height 120)
+                      :height 140)
   ;; 日本語フォントをヒラギノ明朝 Proに
   (set-fontset-font
    nil 'japanese-jisx0208
    ;; 英語名の場合
    ;; (font-spec :family "Hiragino Mincho Pro"))
    (font-spec :family "ヒラギノ明朝 Pro"))
+
   ;; ひらがなとカタカナをモトヤシーダに
   ;; U+3000-303F	CJKの記号および句読点
   ;; U+3040-309F	ひらがな
   ;; U+30A0-30FF	カタカナ
-  (set-fontset-font
-   nil '(#x3040 . #x30ff)
-   (font-spec :family "NfMotoyaCedar"))
+  ;; (set-fontset-font
+  ;;  nil '(#x3040 . #x30ff)
+  ;;  (font-spec :family "NfMotoyaCedar"))
+
   ;; フォントの横幅を調節する
-  (setq face-font-rescale-alist
-        '((".*Menlo.*" . 1.0)
-          (".*Hiragino_Mincho_Pro.*" . 1.2)
-          (".*nfmotoyacedar-bold.*" . 1.2)
-          (".*nfmotoyacedar-medium.*" . 1.2)
-          ("-cdac$" . 1.3))))
+  ;; (setq face-font-rescale-alist
+  ;;       '((".*Menlo.*" . 1.0)
+  ;;         (".*Hiragino_Mincho_Pro.*" . 1.2)
+  ;;         (".*nfmotoyacedar-bold.*" . 1.2)
+  ;;         (".*nfmotoyacedar-medium.*" . 1.2)
+  ;;         ("-cdac$" . 1.3))))
 
 (when (eq system-type 'windows-nt)
   ;; asciiフォントをConsolasに
   (set-face-attribute 'default nil
                       :family "Consolas"
                       :height 120)
+
   ;; 日本語フォントをメイリオに
   (set-fontset-font
    nil
    'japanese-jisx0208
    (font-spec :family "メイリオ"))
+
   ;; フォントの横幅を調節する
   (setq face-font-rescale-alist
         '((".*Consolas.*" . 1.0)
@@ -262,7 +268,7 @@
 (setq show-paren-style 'expression)
 ;; フェイスを変更する
 (set-face-background 'show-paren-match-face nil)
-(set-face-underline-p 'show-paren-match-face "yellow")
+(set-face-underline-p 'show-paren-match-face "green")
 
 
 
